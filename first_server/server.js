5f455a9312de5f0ecfdbfbece4c2a4f12e5cc98a
Tim Combs <tim00combs@gmail.com>
2016-10-17 20:39:00 -0700

    add router.js, update index & server to make sure they work together

--- first_server/server.js
+++ first_server/server.js
@@ -1,11 +1,16 @@
 var http = require('http');
+var url = require('url');
 
 //expanded way to pass a callback to prove async works
-function start() {
+function start(route) {
   function onRequest(request, response) {
-    console.log("Request received.");
+    var pathname = url.parse(request.url).pathname;
+    console.log('Request for ' + pathname + ' received.');
+
+    route(pathname);
+
     response.writeHead(200, {"Content-Type": "text/plain"});
-    response.write("Hello World");
+    response.write('Hello World');
     response.end();
   }
 
