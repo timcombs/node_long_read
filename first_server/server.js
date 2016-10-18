b1fff3835a0c349398bd8be3f47ecee4c63b21e0
Tim Combs <tim00combs@gmail.com>
2016-10-17 20:22:57 -0700

    add index.js and export server functionality to require into index.js

--- first_server/server.js
+++ first_server/server.js
@@ -1,18 +1,22 @@
 var http = require('http');
 
-// //expanded way to pass a callback to prove async works
-// function onRequest(request, response) {
-//   console.log("Request received.");
+//expanded way to pass a callback to prove async works
+function start() {
+  function onRequest(request, response) {
+    console.log("Request received.");
+    response.writeHead(200, {"Content-Type": "text/plain"});
+    response.write("Hello World");
+    response.end();
+  }
+
+  http.createServer(onRequest).listen(8888);
+  console.log('Server has started.');
+}
+
+exports.start = start;
+
+// http.createServer(function(request, response) {
 //   response.writeHead(200, {"Content-Type": "text/plain"});
 //   response.write("Hello World");
 //   response.end();
-// }
-
-// http.createServer(onRequest).listen(8888);
-// console.log('Server has started.');
-
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write("Hello World");
-  response.end();
-}).listen(8888);
+// }).listen(8888);
