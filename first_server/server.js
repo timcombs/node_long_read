1e42dcf54b2465f6fb0fe8aa1584e5c02230acad
Tim Combs <tim00combs@gmail.com>
2016-10-19 23:11:55 -0700

    add request handlers, but not finished

--- first_server/server.js
+++ first_server/server.js
@@ -1,13 +1,12 @@
 var http = require('http');
 var url = require('url');
 
-//expanded way to pass a callback to prove async works
-function start(route) {
+function start(route, handle) {
   function onRequest(request, response) {
     var pathname = url.parse(request.url).pathname;
     console.log('Request for ' + pathname + ' received.');
 
-    route(pathname);
+    route(handle, pathname);
 
     response.writeHead(200, {"Content-Type": "text/plain"});
     response.write('Hello World');
